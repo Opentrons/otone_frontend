9fd40bd75755c12311cd167e593a14954a0250d4
Andy Sigler <andrewsigler1@gmail.com>
2016-07-20 15:15:29 -0400

    formatting

--- main.js
+++ main.js
@@ -52,12 +52,15 @@ function startWampRouter() {
 function startBackend() {
   if (process.platform == "darwin") {
     child_process.exec('pkill -9 \"otone_client\"', function(error, stdout, stderr){
-      backendProcess = child_process.exec(app.getAppPath() + "/backend-dist/mac/otone_client " + app.getAppPath());
+      var backend_command = app.getAppPath() + "/backend-dist/mac/otone_client " + app.getAppPath();
+      backendProcess = child_process.exec(backend_command);
     });
   }
   else if (process.platform == "win32") {
     child_process.exec('taskkill /T /F /IM otone_client.exe',function(error, stdout, stderr){
-      backendProcess = child_process.spawn(app.getAppPath() + "\\backend-dist\\win\\otone_client.exe", [app.getAppPath()]);
+      var backend_path = app.getAppPath() + "\\backend-dist\\win\\otone_client.exe";
+      var backend_arg = app.getAppPath();
+      backendProcess = child_process.spawn(backend_path, [backend_arg]);
     });
   }
   else{
