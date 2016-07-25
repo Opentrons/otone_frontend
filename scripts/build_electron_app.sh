9769380faff47c01ce7f77c5b43d00821d054b6b
Ahmed Abdalla <ahmad19526@gmail.com>
2016-07-25 16:44:47 -0400

    adds electron-prebuilt as deps

--- scripts/build_electron_app.sh
+++ scripts/build_electron_app.sh
@@ -1,1 +1,15 @@
-electron-packager ./ OpenTrons --platform=darwin --arch=x64 --out out --ignore="(node_modules/electron-*|backend)"
+APP_DIR=$PWD
+OUTPUT_DIR="out"
+
+echo "Running electron-packager on $APP_DIR"
+electron-packager $PWD OpenTrons --platform=darwin --arch=x64 --out  --ignore=\
+    "(node_modules/electron-*"\
+    "|backend"\
+    "|scripts"\
+    "|tests"\
+    "|docs"\
+    "|\.node-version"\
+    "|\.python-version"\
+    "|\.git"\
+    "|\.idea"\
+    ")"
