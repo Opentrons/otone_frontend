9685db0e9852cf6eccb339764daacc73f2702f23
nwags <nwags@protonmail.com>
2016-07-25 11:08:53 -0400

    edited npm build script bash file - get directory it is in for reference

--- bin/build.sh
+++ bin/build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-cd ../backend
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR/backend
 pyisntaller otone_client_onefile.spec
 ls -la dist/
 cp dist/otone_client ../backend-dist/Mac
-cd ..
+cd $DIR
 npm install
 npm install -g electron-packager
 electron-packager ./ OpenTrons --platform=darwin --arch=x64 --out releases --ignore="(node_modules/electron-*|backend)"
