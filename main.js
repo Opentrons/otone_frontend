00270aa1d0923874eb88e870007edaa4994a661d
nwags <nwags@protonmail.com>
2016-08-01 11:17:48 -0400

    trying something different - dev tools

--- main.js
+++ main.js
@@ -84,3 +84,12 @@ app.on('activate', function () {
     createWindow()
   }
 })
+
+
+ document.addEventListener("keydown", function (e) {
+  if (e.which === 123) {
+    require('remote').getCurrentWindow().toggleDevTools();
+  } else if (e.which === 116) {
+    location.reload();
+  }
+});
