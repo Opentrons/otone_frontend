9de1b310b731fb07534e514d44572d9e5292d5df
Lev Kravinsky <kravinskylev@gmail.com>
2016-08-02 16:05:11 -0400

    add copy/paste plus various standard menu shortcuts

--- main.js
+++ main.js
@@ -22,16 +22,12 @@ function createWindow () {
   mainWindow.loadURL("file://" + __dirname + "/src/index.html")
 
   mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     mainWindow = null
     app.quit();
   })
 }
 
 app.on('before-quit', function(){
-
   if (process.platform == "darwin") {
     child_process.exec('pkill -9 \"otone_client\"');
   }
@@ -93,9 +89,3 @@ app.on('ready', createWindow)
 app.on('ready', startWampRouter)
 app.on('ready', startBackend)
 app.on('ready', addMenu)
-
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
--- menu.js
+++ menu.js
@@ -4,16 +4,30 @@ const {dialog, Menu, MenuItem, app} = electron;
 const zipFolder = require('zip-folder');
 
 function addMenu() {
-  const template =  [
-    {
-      label: 'Opentrons',
-      submenu: [
-        {
-          label: 'Download Logs',
-          click() { downloadLogs() }
-        }
-      ]
-    }
+  const template =  [{
+    label: "OpenTrons",
+    submenu: [
+        { label: "About", selector: "orderFrontStandardAboutPanel:" },
+        { type: "separator" },
+        { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+    ]}, {
+    label: "Edit",
+    submenu: [
+        { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+        { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+        { type: "separator" },
+        { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+        { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+        { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+        { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+    ]}, {
+    label: 'File',
+    submenu: [
+      {
+        label: 'Download Logs',
+        click() { downloadLogs() }
+      }
+    ]}
   ]
 
   const menu = Menu.buildFromTemplate(template);
