110bf52b9c8a0fa8fb7fb41a34c024d06f31e193
Ahmed Abdalla <ahmad19526@gmail.com>
2016-08-08 17:25:32 -0400

    fixes app path for zipping

--- scripts/zip_ot_app.py
+++ scripts/zip_ot_app.py
@@ -135,7 +135,7 @@ def zip_ot_app(build_tag, os_type):
         zip_command = "powershell.exe -nologo -noprofile -command \"& "
         zip_command += "{ Add-Type -A 'System.IO.Compression.FileSystem'; "
         zip_command += "[IO.Compression.ZipFile]::CreateFromDirectory("
-        zip_command += "'{"+current_app_name+"}','"+zip_app_path+"'); }\""
+        zip_command += "'{" + current_app_path + "}','"+zip_app_path+"'); }\""
         print(script_tab + zip_command)
         zip_process = subprocess.Popen(
             zip_command,
