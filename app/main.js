475e9b8c0c0b388c40ac6697d10f6f116156444d
Nick Wagner <nwags@users.noreply.github.com>
2016-08-16 12:18:56 -0400

    trying code to create/remove shortcuts
    trying code to create/remove shortcuts on installation or removal

--- app/main.js
+++ app/main.js
@@ -9,6 +9,51 @@ const child_process = require('child_process')
 const electron = require('electron')
 const {app, powerSaveBlocker, BrowserWindow} = electron
 
+function handleSquirrelEvent() {
+  if (process.argv.length === 1 && process.platform != "win32") {
+    return false;
+  }
+  const spawn = function(command, args) {
+    let spawnedProcess, error;
+
+    try {
+      spawnedProcess = ChildProcess.spawn(command, args, {detached: true});
+    } catch (error) {}
+
+    return spawnedProcess;
+  };
+  
+  const spawnUpdate = function(args) {
+    return spawn(updateDotExe, args);
+  };
+  
+  const squirrelEvent = process.argv[1];
+  switch (squirrelEvent) {
+    case '--squirrel-install':
+    case '--squirrel-updated':
+      // Optionally do things such as:
+      // - Add your .exe to the PATH
+      // - Write to the registry for things like file associations and
+      //   explorer context menus
+
+      // Install desktop and start menu shortcuts
+      spawnUpdate(['--createShortcut', exeName]);
+
+      setTimeout(app.quit, 1000);
+      return true;
+
+    case '--squirrel-uninstall':
+      // Undo anything you did in the --squirrel-install and
+      // --squirrel-updated handlers
+
+      // Remove desktop and start menu shortcuts
+      spawnUpdate(['--removeShortcut', exeName]);
+
+      setTimeout(app.quit, 1000);
+      return true;
+  }
+};
+
 const addMenu = require('./menu').addMenu;
 const initAutoUpdater = require('./update_helpers').initAutoUpdater;
 
